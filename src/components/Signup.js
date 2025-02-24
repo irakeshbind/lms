a14d5ec3cc7414cb2fab8ec029654a7068f6d0a0
irakeshbind <rakeshbind351998@gmail.com>
2025-02-24 05:52:40 -0600

    signup page crate left side

---
+++ src/components/Signup.js
@@ -0,0 +1,21 @@
+
+
+
+function Signup() {
+  return (
+ <div className="signup-wrapper">
+<div className="signup-box">
+    <div className="signup-left-box">
+        <img alt='logo' src={require('../assets/img1.jpeg')}/>
+        <h1>SBS Online Classes</h1>
+        <p>Learn Coding in Easy Way..</p>
+    </div>
+    <div className="signup-right-box"></div>
+
+
+</div>
+ </div>
+  );
+}
+
+export default Signup
