09b57998ec6440dd67a2de78be098df0e064a672
irakeshbind <rakeshbind351998@gmail.com>
2025-02-24 06:42:32 -0600

    input feature add

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -10,7 +10,21 @@ function Signup() {
         <h1>SBS Online Classes</h1>
         <p>Learn Coding in Easy Way..</p>
     </div>
-    <div className="signup-right-box"></div>
+    <div className="signup-right-box">
+      <h2>Create Your Account</h2>
+      <input placeholder='FullName'/>
+      <textarea placeholder='About me' style={{height:50,borderRadius:5,padding:10, fontSize:15,outline:'none'}}/>
+      {/* // <input placeholder='About me'/> */}
+      <input placeholder='Highest Qualification'/>
+      <input placeholder='Experience'/>
+      <input type='email' placeholder='Email'/>
+      <input placeholder='Phone'/>
+      <input type='password' placeholder='Password'/>
+      <input type='password' placeholder='Confirmpassword'/>
+
+
+      <button className='submit-btn' type='submit'>submit</button>
+    </div>
 
 
 </div>
