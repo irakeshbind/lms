64b65c99326c56d49bd4bb86725f1db08409185b
irakeshbind <rakeshbind351998@gmail.com>
2025-02-26 06:17:04 -0600

    this is add course from include

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -1,4 +1,20 @@
+import React, { useState } from 'react';
+// import ReactQuill from 'react-quill';
+// import 'react-quill/dist/quill.snow.css';
+
 const AddCourse = () => {
+    // const [value, setValue] = useState('');
+    const [profilepic, setProfilePic] = useState(null);
+    const [imageUrl, setImageURl] = useState(null);
+
+
+
+    const fileHandler = (e) => {
+        // console.log(e.target.files[0])
+        setProfilePic(e.target.files[0])
+        setImageURl(URL.createObjectURL(e.target.files[0]))
+    }
+
     return (
         <div className="add-course-wrapper">
             {/* <h2>Add New Course</h2> */}
@@ -7,7 +23,16 @@ const AddCourse = () => {
                 <input placeholder="Price" />
                 <input placeholder="Discount %" />
                 <input placeholder="Description" />
-                <button type="submit">Add Course</button>
+                {/* <ReactQuill theme="snow" value={value} onChange={setValue} /> */}
+                {/* <br/> */}
+                <input id='input-thumb' onChange={fileHandler} type='file' />
+                <div className='upload-profile'>
+                    <button onClick={() => { document.getElementById("input-thumb").click() }} className='upload-profile-btn'>Upload Course Thumbnail</button>
+                    {imageUrl == null ? <img alt='logo' src={require('../assets/img3.png')} />
+                        : <img alt='logo' src={imageUrl} />}
+                </div>
+
+                <button className='add-course-button' type="submit">Add Course</button>
             </form>
         </div>
     )
