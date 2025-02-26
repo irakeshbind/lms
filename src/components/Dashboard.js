4a1ff5f8b7596d917a28ffef6cbc1e520f4f50db
irakeshbind <rakeshbind351998@gmail.com>
2025-02-25 23:49:46 -0600

    add the icons in menubar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -13,9 +13,9 @@ const Dashboard = () => {
                     </div>
                 </div>
                 <div className="menu">
-                    <Link to='/dashboard/home' className={location.pathname === '/dashboard/home' ? 'menu-active-link' : 'menu-link'}><i class="fa fa-home" aria-hidden="true"></i>Home</Link>
-                    <Link to='/dashboard/my-courses' className={location.pathname === '/dashboard/my-courses' ? 'menu-active-link' : 'menu-link'}>My Courses</Link>
-                    <Link to='/dashboard/add-course' className={location.pathname === '/dashboard/add-course' ? 'menu-active-link' : 'menu-link'}>Add New Course</Link>
+                    <Link to='/dashboard/home' className={location.pathname === '/dashboard/home' ? 'menu-active-link' : 'menu-link'}><i className="fa fa-home" aria-hidden="true"></i>Home</Link>
+                    <Link to='/dashboard/my-courses' className={location.pathname === '/dashboard/my-courses' ? 'menu-active-link' : 'menu-link'}><i className="fa fa-book" aria-hidden="true"></i>My Courses</Link>
+                    <Link to='/dashboard/add-course' className={location.pathname === '/dashboard/add-course' ? 'menu-active-link' : 'menu-link'}><i className="fa fa-plus" aria-hidden="true"></i>Add New Course</Link>
                   
                 </div>
             </div>
