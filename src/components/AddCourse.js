e3c82475dcb1a496e28ce514acce27c6b32ae59a
irakeshbind <rakeshbind351998@gmail.com>
2025-02-27 00:17:37 -0600

    add the react quill editors

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -3,33 +3,39 @@ import React, { useState } from 'react';
 // import 'react-quill/dist/quill.snow.css';
 
 const AddCourse = () => {
-    // const [value, setValue] = useState('');
-    const [profilepic, setProfilePic] = useState(null);
+    const [description, setDescription] = useState('');
+    const [courseName, setCourseName] = useState('');
+    const [price, setPrice] = useState(null);
+    const [discount, setDiscount] = useState(null);
+    const [thumbnail, setThumbnail] = useState(null);
     const [imageUrl, setImageURl] = useState(null);
 
 
 
     const fileHandler = (e) => {
         // console.log(e.target.files[0])
-        setProfilePic(e.target.files[0])
+        setThumbnail(e.target.files[0])
         setImageURl(URL.createObjectURL(e.target.files[0]))
+    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(courseName, description, price, discount, thumbnail)
     }
 
     return (
         <div className="add-course-wrapper">
-            {/* <h2>Add New Course</h2> */}
-            <form className="add-course-form">
-                <input placeholder="Course Name" />
-                <input placeholder="Price" />
-                <input placeholder="Discount %" />
-                <input placeholder="Description" />
-                {/* <ReactQuill theme="snow" value={value} onChange={setValue} /> */}
+            {/* <h2>Add New Course                         </h2> */}
+            <form onSubmit={submitHandler} className="add-course-form">
+                <input onChange={(e)=>{setCourseName(e.target.value)}} placeholder="Course Name" />
+                <input onChange={(e)=>{setPrice(e.target.value)}} placeholder="Price" />
+                <input onChange={(e)=>{setDiscount(e.target.value)}} placeholder="Discount %" />
+                <input onChange={(e)=>{setDescription(e.target.value)}} placeholder="Description" />
+                {/* <ReactQuill theme="snow" value={description} onChange={setDescription} /> */}
                 {/* <br/> */}
                 <input id='input-thumb' onChange={fileHandler} type='file' />
-                <div className='upload-profile'>
+                <div className='upload-thumb'>
                     <button onClick={() => { document.getElementById("input-thumb").click() }} className='upload-profile-btn'>Upload Course Thumbnail</button>
-                    {imageUrl == null ? <img alt='logo' src={require('../assets/img3.png')} />
-                        : <img alt='logo' src={imageUrl} />}
+                    {imageUrl && <img alt='logo' src={imageUrl} />}
                 </div>
 
                 <button className='add-course-button' type="submit">Add Course</button>
