458e935035693dded0a802700c58c1031d9728f2
irakeshbind <rakeshbind351998@gmail.com>
2025-02-28 00:29:22 -0600

    therer are edit and delete button

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -1,12 +1,28 @@
 import { useLocation, useParams } from "react-router-dom"
 
-const CourseDetails =()=>{
-    const {id} = useParams();
+const CourseDetails = () => {
+    const { id } = useParams();
     console.log(id)
     const location = useLocation()
     console.log(location.state)
-    return(
-        <h1>courseDetails</h1>
-      )
-  }
-  export default CourseDetails
+    return (
+        <div className="course-detail-wrapper">
+            <div className="cd-header">
+                <div className="cd-header-left">
+                    <h2 className="cd-heading">MERN Stack Complete Master Course</h2>
+                    <p>Created At :-23/10/2025</p>
+                    <div className="cd-header-btn-box">
+                        <button className="cd-edit-btn">Edit coursee</button>
+                        <button className="cd-delete-btn">Delete course</button>
+                    </div>
+                </div>
+                <div className="cd-header-right">
+                    <img className="cd-thumbnail" src={location.state.thumbnail}/>
+                    <button>Manage Content</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default CourseDetails
--- src/components/MyCourses.js
+++ src/components/MyCourses.js
@@ -8,70 +8,80 @@ const MyCourses =()=>{
             courseName:'c++ master course',
             price:500,
             discount:20,
-            thumbnail:'https://gogetgpt.com/files/img/1beb834ea28aa4d66bfd3852aea26c51.png'
+            thumbnail:'https://gogetgpt.com/files/img/1beb834ea28aa4d66bfd3852aea26c51.png',
+            description:'part 1'
         },
         {
             id:1,
             courseName:'python master course',
             price:3999,
             discount:10,
-            thumbnail:'https://thecodex.me/static/52b74df4524162e8807bea5fb094d53c/ee604/Python_Thumbnail_b6e99c74df.png'
+            thumbnail:'https://thecodex.me/static/52b74df4524162e8807bea5fb094d53c/ee604/Python_Thumbnail_b6e99c74df.png',
+            description:'part 1'
         },
         {
             id:2,
             courseName:'iot from basic to advance',
             price:7999,
             discount:50,
-            thumbnail:'https://static.vecteezy.com/system/resources/thumbnails/009/288/681/small/iot-internet-of-things-logo-in-the-shape-of-sphere-with-hexagon-pattern-in-wireframe-hand-everything-connectivity-device-concept-network-and-business-with-internet-illustration-vector.jpg'
+            thumbnail:'https://static.vecteezy.com/system/resources/thumbnails/009/288/681/small/iot-internet-of-things-logo-in-the-shape-of-sphere-with-hexagon-pattern-in-wireframe-hand-everything-connectivity-device-concept-network-and-business-with-internet-illustration-vector.jpg',
+            description:'part 1'
         },
         {
             id:3,
             courseName:'Reactnaive',
             price:5999,
             discount:10,
-            thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyQRQtqf28ZmSfOqurZc6LjEkfuxBSsBOSIg&s'
+            thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyQRQtqf28ZmSfOqurZc6LjEkfuxBSsBOSIg&s',
+            description:'part 1'
         },
         {
             id:4,
             courseName:'mern stack',
             price:4099,
             discount:3,
-            thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1XsGy58iVDkzGX91ESK0nfFB6teEIheOYOg&s'
+            thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1XsGy58iVDkzGX91ESK0nfFB6teEIheOYOg&s',
+            description:'part 1'
         },
         {
             id:0,
             courseName:'c++ master course',
             price:5999,
             discount:20,
-            thumbnail:'https://gogetgpt.com/files/img/1beb834ea28aa4d66bfd3852aea26c51.png'
+            thumbnail:'https://gogetgpt.com/files/img/1beb834ea28aa4d66bfd3852aea26c51.png',
+            description:'part 1'
         },
         {
             id:1,
             courseName:'python master course',
             price:3999,
             discount:10,
-            thumbnail:'https://thecodex.me/static/52b74df4524162e8807bea5fb094d53c/ee604/Python_Thumbnail_b6e99c74df.png'
+            thumbnail:'https://thecodex.me/static/52b74df4524162e8807bea5fb094d53c/ee604/Python_Thumbnail_b6e99c74df.png',
+            description:'part 1'
         },
         {
             id:2,
             courseName:'iot from basic to advance',
             price:7999,
             discount:50,
-            thumbnail:'https://static.vecteezy.com/system/resources/thumbnails/009/288/681/small/iot-internet-of-things-logo-in-the-shape-of-sphere-with-hexagon-pattern-in-wireframe-hand-everything-connectivity-device-concept-network-and-business-with-internet-illustration-vector.jpg'
+            thumbnail:'https://static.vecteezy.com/system/resources/thumbnails/009/288/681/small/iot-internet-of-things-logo-in-the-shape-of-sphere-with-hexagon-pattern-in-wireframe-hand-everything-connectivity-device-concept-network-and-business-with-internet-illustration-vector.jpg',
+            description:'part 1'
         },
         {
             id:3,
             courseName:'Reactnaive',
             price:5999,
             discount:10,
-            thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyQRQtqf28ZmSfOqurZc6LjEkfuxBSsBOSIg&s'
+            thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyQRQtqf28ZmSfOqurZc6LjEkfuxBSsBOSIg&s',
+            description:'part 1'
         },
         {
             id:4,
             courseName:'mern stack',
             price:4099,
             discount:3,
-            thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1XsGy58iVDkzGX91ESK0nfFB6teEIheOYOg&s'
+            thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1XsGy58iVDkzGX91ESK0nfFB6teEIheOYOg&s',
+            description:'part 1'
         }
     ]
     return(
