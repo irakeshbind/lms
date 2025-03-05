692af812ea3569ca330d68a2046cf8d7745c7653
irakeshbind <rakeshbind351998@gmail.com>
2025-03-05 04:55:16 -0600

    add in this course and student

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,32 @@
-const Home =()=>{
-    return(
-        <h1>home</h1>
+const Home = () => {
+    return (
+        <div className="home-wrapper">
+            <div className="header">
+                <div className="box-1">
+                 <img src={require('../assets/img66.png')}/>
+                    <div className='sp-home'>
+                        <h1>125</h1>
+                        <p>courses</p>
+                    </div>
+                </div>
+                <div className="box-2">
+                    <img src={require('../assets/img55.png')} />
+                    <div className='sp-home'>
+                        <h1>1456</h1>
+                        <p>student</p>
+                    </div>
+
+                </div>
+                <div className="box-3">
+                    <img src={require('../assets/img44.png')} />
+                    <div className='sp-home'>
+                        <h1>RS. 452163</h1>
+                        <p>Total Transaction</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 export default Home
