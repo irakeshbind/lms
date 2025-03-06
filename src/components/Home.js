fb0396c3340c83dab1403e19fe686a3a37434c18
irakeshbind <rakeshbind351998@gmail.com>
2025-03-06 01:17:41 -0600

    studuent table text  copmlete

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,6 +14,60 @@ import{
 
 Chartjs.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 const Home = () => {
+    const courses = [
+        {
+            id:0,
+            courseName:'c++ master course',
+            price:500,
+            discount:20,
+            thumbnail:'https://gogetgpt.com/files/img/1beb834ea28aa4d66bfd3852aea26c51.png',
+            description:'AddCourse.js:22  What is C++ used for? Over time, C++ has become a choice language for building fast and reliable software. Its versatility makes it an excellent option for complex applications, embedded systems, and Internet of Things (IoT) devices. A few common areas where you may opt for C++ include:  Software engineering. A few applications you might use daily that utilize C++ include Netflix and Amazon.  Operating system (OS) development uses C++. Examples of OSs that use C++ include Microsoft Windows, Mac OS X, and iOS.  Graphical user interfaces. C++ is often useful for graphical user interface design or GUI.  Virtual reality (VR). C++ can be an excellent language to learn if you want to develop VR applications.  Blockchain technology. The well-known cryptocurrency Bitcoin was developed in C++. You can also use it to create smart contracts.  Game development. You can develop games and gaming consoles with C++. For example, the game World of Warcraft and the Xbox console utilize C++'
+        },
+        {
+            id:1,
+            courseName:'python master course',
+            price:3999,
+            discount:10,
+            thumbnail:'https://thecodex.me/static/52b74df4524162e8807bea5fb094d53c/ee604/Python_Thumbnail_b6e99c74df.png',
+            description:'AddCourse.js:22  What is C++ used for? Over time, C++ has become a choice language for building fast and reliable software. Its versatility makes it an excellent option for complex applications, embedded systems, and Internet of Things (IoT) devices. A few common areas where you may opt for C++ include:  Software engineering. A few applications you might use daily that utilize C++ include Netflix and Amazon.  Operating system (OS) development uses C++. Examples of OSs that use C++ include Microsoft Windows, Mac OS X, and iOS.  Graphical user interfaces. C++ is often useful for graphical user interface design or GUI.  Virtual reality (VR). C++ can be an excellent language to learn if you want to develop VR applications.  Blockchain technology. The well-known cryptocurrency Bitcoin was developed in C++. You can also use it to create smart contracts.  Game development. You can develop games and gaming consoles with C++. For example, the game World of Warcraft and the Xbox console utilize C++'
+        },
+        {
+            id:2,
+            courseName:'iot basic to advance',
+            price:7999,
+            discount:50,
+            thumbnail:'https://static.vecteezy.com/system/resources/thumbnails/009/288/681/small/iot-internet-of-things-logo-in-the-shape-of-sphere-with-hexagon-pattern-in-wireframe-hand-everything-connectivity-device-concept-network-and-business-with-internet-illustration-vector.jpg',
+            description:'AddCourse.js:22  What is C++ used for? Over time, C++ has become a choice language for building fast and reliable software. Its versatility makes it an excellent option for complex applications, embedded systems, and Internet of Things (IoT) devices. A few common areas where you may opt for C++ include:  Software engineering. A few applications you might use daily that utilize C++ include Netflix and Amazon.  Operating system (OS) development uses C++. Examples of OSs that use C++ include Microsoft Windows, Mac OS X, and iOS.  Graphical user interfaces. C++ is often useful for graphical user interface design or GUI.  Virtual reality (VR). C++ can be an excellent language to learn if you want to develop VR applications.  Blockchain technology. The well-known cryptocurrency Bitcoin was developed in C++. You can also use it to create smart contracts.  Game development. You can develop games and gaming consoles with C++. For example, the game World of Warcraft and the Xbox console utilize C++s'
+        }
+    ]
+    const studentlist = [
+        {
+
+            id: 125,
+            fullName: 'rakesh',
+            phone: '9326827804',
+            email: 'rakesh@gmail.com',
+            profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEpKvJ-2mw_38DdZLgdwmmCgjpu3RvKGHqkA&s'
+        },
+        {
+
+            id: 165,
+            fullName: 'ajit',
+            phone: '8354063869',
+            email: 'ajit@gmail.com',
+            profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBdl1Qi4s61DFzf292IYutoG5QU9JxE4YxFA&s'
+        },
+        {
+
+            id: 170,
+            fullName: 'amar',
+            phone: '9592784519',
+            email: 'amar@gmail.com',
+            profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYqpXqhUvAxSKy5POIO9WXcy_QdF81eVLSqQ&s'
+        }
+     
+
+    ]
   
     const data ={
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
@@ -75,10 +129,38 @@ const Home = () => {
                  </div>
                  <div className='recent-box'>
                     <div className='r-student-box'>
-                         student
+                    <table className='recent-box-table'>
+                        <tbody>
+                            {
+                                studentlist.map((student)=>{
+                                    return(
+                                        <tr key={student.id}>
+                                           <td><img src={student.profilePic}/></td>
+                                           <td>{student.fullName}</td>
+                                           <td>{student.phone}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
                     </div>
                     <div className='r-course-box'>
-                          course
+                    <table className='recent-box-table'>
+                        <tbody>
+                            {
+                                courses.map((course)=>{
+                                    return(
+                                        <tr key={course.id}>
+                                           <td><img src={course.thumbnail}/></td>
+                                           <td>{course.courseName}</td>
+                                           <td>{course.price}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
                     </div>
                  </div>
             </div>
