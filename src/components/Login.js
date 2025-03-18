93a4ca54c2e6128009e5d5c96416258f6cc73add
irakeshbind <rakeshbind351998@gmail.com>
2025-03-18 02:03:03 -0500

    login api complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,42 @@
+import axios from 'axios';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 
 function Login() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isloading,setLoading] = useState(false)
+     
+    const navigate = useNavigate();
 
     const submitHandler = (e) => {
-        e.preventDefault()
-        console.log(email, password);
-    }
+        e.preventDefault();
+        setLoading(true)
+        // console.log(email, password);
+
+        axios.post('http://localhost:4200/auth/admin/login',{
+            email:email,
+            password:password
+        })
+        .then(res=>{
+            setLoading(false)
+          console.log(res);
+          toast('welcome to sbs online classes')
+          
+          navigate('/dashboard')
+          
+        })
+        .catch(err=>{
+          setLoading(false)
+          toast.error('something is wrong')
+          console.log(err)
+         
+        })
+      }
+    
 
     return (
         <div className="signup-wrapper">
@@ -24,7 +50,7 @@ function Login() {
                     <h2>Login with Your Account</h2>
                     <input onChange={(e) => { setEmail(e.target.value) }} type='email' placeholder='Email' />
                     <input onChange={(e) => { setPassword(e.target.value) }} type='password' placeholder='Password' />
-                    <button className='submit-btn' type='submit'>submit</button>
+                    <button className='submit-btn' type='submit'>{isloading &&   <i class="fa-solid fa-spinner fa-spin-pulse fa-spin-reverse"></i>}submit</button>
                     <Link to='/signup' className='link'>i dont account</Link>
                 </form>
 
